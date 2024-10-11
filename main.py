2f835d620ed2f192523a6e2641ddbb3c63e224f6
jparep <parepson@gmail.com>
2024-10-11 00:08:50 -0600

    Set up chrome options for selenium

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Import Libraries
 import time
 import requests
 import pandas as pd
@@ -7,3 +8,9 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+
+# Set up Chrome options for Selenium
+chrome_options = Options()
+chrome_options.add_arguement("--headless") # Run in headless mode (no UI)
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--diasble-dev-shm-usage")
