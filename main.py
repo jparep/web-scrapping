57d9b3159629cbdc0e52d2c5648f274eb105a9bd
jparep <parepson@gmail.com>
2024-10-11 22:01:09 -0600

    Set up Selenium for handling Javascript-driven content

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 # Import Libraries
-import time
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
@@ -8,9 +7,13 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+import time
 
-# Set up Chrome options for Selenium
-chrome_options = Options()
-chrome_options.add_argument("--headless") # Run in headless mode (no UI)
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--diasble-dev-shm-usage")
+# Setting up Selenium for hadling JavaScript-driven content
+def setup_driver():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless") # Run in headless mode (no UI)
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--diasble-dev-shm-usage")
+    
+    service = Service(ChromeDriverManager().install())
