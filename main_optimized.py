0505f103118821310bd0d017eb51ca122901d0a2
jparep <parepson@gmail.com>
2024-10-24 23:13:48 -0600

    created main optimized file to use class

---
+++ main_optimized.py
@@ -0,0 +1,10 @@
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+import logging
